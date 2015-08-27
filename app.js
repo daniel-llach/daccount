d1e8177bbfbc2bfe5c4fa711497e4a353dd70aef
daniel-llach <daniel.llach@gmail.com>
2015-08-27 13:42:52 -0300

    config ready

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var colors = require('colors');
 var url = require('url'); // req.body
+var config = require('./config');
 
 /*
  * Create Express app
@@ -47,21 +48,25 @@ var server = app.listen(4000, function() {
  var mysql = require('mysql');
 
  // DB connect
- var dbconfig = require('./db/config');
  var connection = mysql.createConnection({
-  host     : dbconfig.host,
-  user     : dbconfig.user,
-  password : dbconfig.password,
-  database : dbconfig.database
+  host     : config.db_host,
+  user     : config.db_user,
+  password : config.db_password,
+  database : config.db_database
  });
 // register form
 app.get('/register', function(req, res){
  // get rols
  connection.query('SELECT * FROM `rols`' , function(err, rols) {
-   res.render('register', {data: {rols:rols} });
+   res.render('register', {
+     url_faccount: config.url_faccount,
+     data: {rols:rols}
+   });
  });
 });
 // login form
 app.get('/login', function(req, res){
-  res.render('login');
+  res.render('login', {
+    url_daccount: config.url_daccount
+  });
 });
