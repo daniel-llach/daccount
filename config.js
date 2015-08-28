e694c88a752068dde2cac137febc1456566bbf08
daniel-llach <daniel.llach@gmail.com>
2015-08-28 12:53:51 -0300

    config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 module.exports.url_agendador = 'http://localhost:3000';
 module.exports.url_daccount = 'http://localhost:4000';
 module.exports.url_faccount = 'http://localhost:5000';
-module.exports.url_darwined = 'http://localhost/darwined/big_branch';
+// module.exports.url_darwined = 'http://localhost/darwined/big_branch';
 
 /*
  * CONFIG DATABASE
