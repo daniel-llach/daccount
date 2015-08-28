7853d743d853802677e503262e9923eb538b06bd
daniel-llach <daniel.llach@gmail.com>
2015-08-28 13:04:15 -0300

    bd url

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
  * Routers
  */
 module.exports.url_agendador = 'http://localhost:3000';
-module.exports.url_daccount = 'http://localhost:4000';
+module.exports.url_daccount = 'daccount.herokuapp.com';
 module.exports.url_faccount = 'faccount.herokuapp.com';
 // module.exports.url_darwined = 'http://localhost/darwined/big_branch';
 
 /*
  * CONFIG DATABASE
  */
-module.exports.db_host = '192.168.0.150';
+module.exports.db_host = '186.67.218.180';
 module.exports.db_user = 'root';
 module.exports.db_password = 'foris123'
 module.exports.db_database = 'foris_account'
