a6510c6dbf4c36c230d7cb2e32b40065f84d36e4
daniel-llach <daniel.llach@gmail.com>
2015-08-31 10:46:49 -0300

    url fixed

--- config.js
+++ config.js
@@ -3,7 +3,6 @@
  */
 module.exports.url_daccount = 'https://daccount.herokuapp.com';
 module.exports.url_faccount = 'https://faccount.herokuapp.com';
-// module.exports.url_darwined = 'http://localhost/darwined/big_branch';
 
 /*
  * CONFIG DATABASE
